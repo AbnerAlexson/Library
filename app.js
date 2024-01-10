39ffa7e56da80b5f08ee886e1167597fee4e9116
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2024-01-10 18:35:34 +0800

    update app.js
    created popup function for add new book form
    
    add a function that stores books

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 const myLibrary = [];
 
-function Book() {
-    // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
-    //do stuff here
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').checked;
+    let NewBook = new Book(title, author, pages, read);
+    myLibrary.push(NewBook); 
 }
 
- let addBookbtn = document.querySelector('#new-book-btn');
- addBookbtn.addEventListener("click", () => {
-      let addBookForm = document.querySelector("#new-book-form");
-      addBookForm.style.display = "block";
+    // button that for add book form to appear
+let addBookbtn = document.querySelector('#new-book-btn');
+addBookbtn.addEventListener("click", () => {
+      let addBookForm = document.querySelector("#popup-container-inactive");
+      addBookForm.setAttribute('id', 'popup-container-active')
+});
+
+    // button to close the add book function
+let closeAddBookBtn = document.querySelector(".close-btn");
+closeAddBookBtn.addEventListener('click', () => {
+    let addBookForm = document.querySelector("#popup-container-active");
+    addBookForm.setAttribute('id', 'popup-container-inactive');
+});
+
+    //function as a button calls in a function that store books into myLibrary
+let submitNewBook = document.querySelector('.submitBtn > input');
+submitNewBook.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBookToLibrary();
 })
